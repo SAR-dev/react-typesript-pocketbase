cc91254077e0398cd78be34f12f71220d7b9bfd7
Sayed Rafi <sayedarrafi.bd@gmail.com>
2024-09-26 01:39:27 +0900

    up

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,53 @@
 import { useState } from "react"
 
 function App() {
-  const [username, setUsername] = useState("")
+  const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
+  const [message, setMessage] = useState("")
 
   const handleSubmit = () => {
-    console.log(username)
-    console.log(password)
+    // API endpoint for creating a new user
+    const apiUrl = 'http://127.0.0.1:8090/api/collections/users/records';
+
+    // Form data to be sent in the request body
+    const formData = {
+      email: email,
+      password: password,
+      passwordConfirm: password,
+    };
+
+    // Make a POST request using the Fetch API
+    fetch(apiUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(newUserData => {
+        setEmail("")
+        setPassword("")
+        setMessage("Account has been created")
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
   }
 
   return (
     <div className="h-screen w-full flex justify-center items-center">
       <div className="grid grid-cols-1 gap-3 w-96 p-5 rounded-lg border border-gray-300">
       <div className="text-xl font-semibold text-center">Welcome To Keep Clone</div>
-      <input className="border border-gray-300 p-3 rounded bg-white" type="text" placeholder="username" value={username} onChange={e => setUsername(e.target.value)} />
+      <input className="border border-gray-300 p-3 rounded bg-white" type="text" placeholder="email" value={email} onChange={e => setEmail(e.target.value)} />
       <input className="border border-gray-300 p-3 rounded bg-white" type="password" placeholder="password" value={password} onChange={e => setPassword(e.target.value)} />
       <button className="w-28 rounded bg-blue-500 py-3 mx-auto" onClick={handleSubmit}>Submit</button>
+      <div>{message}</div>
       </div>
     </div>
   )
